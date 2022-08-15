3f129296b89b3fea857945dc7b09c9a2cf19fc27
FrankyHErus <frankyherus@yandex.ru>
2022-08-15 14:18:48 +0300

    commit6

--- dist/html/Chat.0130762b.js
+++
@@ -1,7 +0,0 @@
-Handlebars.registerHelper("for", function(n, block) {
-    let result = "";
-    for(let i = 0; i < n; i++)accum += block.fn(i);
-    return accum;
-});
-
-//# sourceMappingURL=Chat.0130762b.js.map
